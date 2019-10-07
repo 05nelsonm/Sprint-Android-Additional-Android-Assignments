9a6fa4e94075680d6934aab68b6476d4c423cf06
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-07 12:50:32 -0400

    setup the playOrPauseFunctionality method call

--- M01/Guided Project/MediaProgramming/app/src/main/java/com/lambdaschool/mediaprogramming/MainActivity.kt
+++ M01/Guided Project/MediaProgramming/app/src/main/java/com/lambdaschool/mediaprogramming/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_exo_player.view.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 /*
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //TODO 1: Disable the play/pause button by default (you'll enable it later)
+        play_pause_button.isEnabled = false
 
         //TODO 2: Add the play/pause functionality including the animation for the button and
         //TODO playing or pausing the VideoView.
@@ -36,11 +38,29 @@ class MainActivity : AppCompatActivity() {
         // If the video is not playing, start it
         // else pause the video
         // Start the animation
+
+        play_pause_button.setOnClickListener {
+            if (video_view.isPlaying) {
+                // pause
+                video_view.pause()
+                play_pause_button.setImageDrawable(getDrawable(R.drawable.avd_anim_pause_play))
+            } else {
+                // play
+                video_view.start()
+                play_pause_button.setImageDrawable(getDrawable(R.drawable.avd_anim_play_pause))
+            }
+
+            val drawable = play_pause_button.drawable
+            if (drawable is Animatable) {
+                (drawable as Animatable).start()
+            }
+        }
     }
 
     private fun seekBarFunctionality() {
         // In the SeekBar listener, when the seekbar progress is changed,
         // update the video progress
+        
     }
 
     override fun onStart() {
